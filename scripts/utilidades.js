2278c15cbea0de176f4306d82102621e5e5e8142
dalaiboada <dalaiboadajob@gmail.com>
2025-10-17 01:38:07 -0400

    feat(utilidades): Implementar funciones para manipulación de caracteres y registro de pasos en la encriptación

---
+++ scripts/utilidades.js
@@ -0,0 +1,26 @@
+import { ALFABETO } from "./constantes.js";
+
+function getIndice(caracter){ return ALFABETO.indexOf(caracter); }
+
+function getCaracter(indice){ return ALFABETO[(indice + 26) % 26]; }
+
+function convertirAIndices(cadena) { 
+	return cadena.split('').map(caracter => 
+		getIndice(caracter)
+	);
+}
+
+/**
+ * Función auxiliar para registrar un paso en el camino (pasos) de codificación.
+ * @param {Array} pasos	 - El array donde se registran los pasos.
+ * @param {string} nombreEtapa - Nombre legible de la etapa (ej: 'Rotor Medio →').
+ * @param {number} nuevoIndice - El índice (0-25) resultante de la operación.
+ */
+function registrarPaso(pasos, nombreEtapa, nuevoIndice) {
+    pasos.push({
+      paso: nombreEtapa,
+      valor: getCaracter(nuevoIndice)
+    });
+}
+
+export { getIndice, getCaracter, convertirAIndices, registrarPaso };
