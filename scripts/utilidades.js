f1d40c76d07726168252133de3892595fbdfcde7
Daniel Rodriguez <danielrxd0912@gmail.com>
2025-10-23 18:44:04 -0400

    Documentar utilidades.js

--- scripts/utilidades.js
+++ scripts/utilidades.js
@@ -1,22 +1,24 @@
 import { ALFABETO } from "./constantes.js";
 
+// Devuelve el indice del carácter en el alfabeto
 const getIndice = caracter => ALFABETO.indexOf(caracter); 
 
+// Devuelve el carácter correspondiente al indice
 const getCaracter = indice => ALFABETO[(indice + 26) % 26]; 
 
+// Devuelve un array de indices correspondientes a la cadena
 const convertirAIndices = cadena => { 
 	return cadena.split('').map(caracter => 
 		getIndice(caracter)
 	);
 }
 
-/**
- * Función auxiliar para registrar un paso en el camino (pasos) de codificación.
- * @param {Array} pasos	 - El array donde se registran los pasos.
- * @param {string} nombreEtapa - Nombre legible de la etapa (ej: 'Rotor Medio →').
- * @param {number} nuevoIndice - El índice (0-25) resultante de la operación.
- */
-const registrarPaso = (pasos, nombreEtapa, nuevoIndice) => {
+// Registra un paso en el array de pasos
+const registrarPaso = (
+  pasos, // Array de pasos { paso: string, valor: string }
+  nombreEtapa, // Nombre legible de la etapa (ej: 'Rotor Medio →')
+  nuevoIndice, // El índice (0-25) resultante de la operación
+) => {
   pasos.push({
     paso: nombreEtapa,
     valor: getCaracter(nuevoIndice)
